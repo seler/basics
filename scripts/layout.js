a46a3a32676657fb39a2d87f2525378e48c29d49
Rafał Selewońko <rafal@selewonko.com>
2011-05-26 13:20:31 +0200

    footer i nawigacja w footerze

---
+++ scripts/layout.js
@@ -0,0 +1,16 @@
+/* layout.js */
+
+/**********************************************/
+/*** basics - the ultimate html, css and js ***/
+/*** framework for instant web page         ***/
+/**********************************************/
+/*** Rafa³ Selewoñko <rafal@selewonko.com>  ***/
+/**********************************************/
+
+
+$(document).ready(function() {
+    $('.slideshow').cycle({
+		fx: 'fade',
+		timeout: 5000
+	});
+});
